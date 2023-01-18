8a037c307e34a5bf01d565baaf14ab87c19a96db
unknown <artem4rolov@gmail.com>
2023-01-18 05:54:02 +0300

    begin

---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+const navLinks = [
+  { id: "Home", title: "Home" },
+  { id: "About Us", title: "About Us" },
+  { id: "Features", title: "Features" },
+  { id: "Solution", title: "Solution" },
+];
